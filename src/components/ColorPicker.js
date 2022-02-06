2250ed369c9482dc25d5ce9cd6e49df9ab35ecee
Pr0lamer <andrewmakmail@gmail.com>
2022-02-07 01:44:37 +0200

    cancel and submit buttons functions

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -6,15 +6,12 @@ export default function ColorPicker(){
         {
             red: 50,
             green: 50,
-            blue: 50
+            blue: 50,
+            isShown: true
         }
     );
-    //const square = document.querySelector(".square");
-
+ 
     function print(event){
-        //const p = document.querySelector("p");
-        //p.innerHTML = e.target.value;
-        //console.log(e.target.value);
         
         setValue(prevValue =>{
             return {
@@ -25,20 +22,60 @@ export default function ColorPicker(){
         
     }
 
+    let prevCount = usePrevious(value);
+    
+    function usePrevious(value) {
+        // The ref object is a generic container whose current property is mutable ...
+        // ... and can hold any value, similar to an instance property on a class
+        const ref = React.useRef();
+        // Store current value in ref
+        React.useEffect(() => {
+          ref.current = value;
+        }, [value]); // Only re-run if value changes
+        // Return previous value (happens before update in useEffect above)
+        return ref.current;
+      }
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        //prevCount = usePrevious(value);
+
+        setValue(prevValue =>{
+            return {
+                ...prevValue,
+                [event.target.name]: event.target.value
+            };
+        });
+    }
+
+
+    function onCancel(){
+        setValue(prevValue =>{
+            return {
+                ...prevValue,
+                ...prevCount
+            };
+        });
+        console.log(prevCount);
+    }
+
+
+    
 
     return (
         <>
-            <div className="square" 
-            style={{background: `rgb(${value.red}, ${value.green}, ${value.blue})`}}></div>
+            <div className="square" style={{backgroundColor: `rgb(${value.red}, ${value.green}, ${value.blue})`}}></div>
            
-            <form className="form">
-                <RangeInput name="red" handleOnInput={print} value={value.red}/>
+            <form className="form" onSubmit={handleSubmit}>
+                <RangeInput name="red" handleOnChange={print} value={value.red}/>
                 
-                <RangeInput name="green" handleOnInput={print} value={value.green}/>
-                <RangeInput name="blue" handleOnInput={print} value={value.blue}/>
+                <RangeInput name="green" handleOnChange={print} value={value.green}/>
+                <RangeInput name="blue" handleOnChange={print} value={value.blue}/>
                 <div className="form--buttons-box">
-                    <button className="form--buttons">Cancel</button>
-                    <button type="submit" className="form--buttons">Submit</button>
+                    <button type="button" className="form--buttons" onClick={onCancel}>Cancel</button>
+                    <button className="form--buttons">Submit</button>
+                    
                 </div>
                 
             </form>
--- src/components/RangeInput.js
+++ src/components/RangeInput.js
@@ -4,11 +4,12 @@ export default function RangeInput(props){
 
 
 
+
     return(
         <div className="form--input-box">
-            <input min={0} max={255} type="range" className="form--input" onChange={props.handleOnInput}
-             name={props.name}/>
-            <p>{props.value}</p>
+            <input min={0} max={255} type="range" className="form--input" onChange={props.handleOnChange}
+             name={props.name} value={props.value}/>
+            <label>{props.value}</label>
         </div>
         
     );
